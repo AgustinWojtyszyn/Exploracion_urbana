7f3268ba20c997f8766d3acdc271656f8ed9e8a6
Agustin Wojtysyzn <agustinwojtyszyn99@gmail.com>
2026-09-19 02:33:10 -0300

    style: load dedicated Leyenda type system

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource/barlow-condensed/600.css'
+import '@fontsource/barlow-condensed/700.css'
+import '@fontsource/manrope/400.css'
+import '@fontsource/manrope/600.css'
+import '@fontsource/manrope/700.css'
 import { App } from './App'
 import './styles.css'
 
