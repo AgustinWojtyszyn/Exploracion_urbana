3d4f99bc1f133c46e5aa5fe67cf8a200fb27d0d9
Agustin Wojtysyzn <agustinwojtyszyn99@gmail.com>
2026-09-19 01:13:25 -0300

    refactor: retire old prototype world

--- src/world/PrototypeWorld.tsx
+++ src/world/PrototypeWorld.tsx
@@ -1,13 +1,1 @@
-import { Station } from './zones/Station'
-import { Gallery } from './zones/Gallery'
-import { Maintenance } from './zones/Maintenance'
-import { Below } from './zones/Below'
-import { StationDetails } from './StationDetails'
-
-export function PrototypeWorld() {
-  return <>
-    <ambientLight intensity={0.35} color="#8eaaa9" />
-    <directionalLight position={[8, 18, 10]} intensity={0.7} color="#b6c4d5" />
-    <Station /><Gallery /><Maintenance /><Below /><StationDetails />
-  </>
-}
+export { Tower17World as PrototypeWorld } from './Tower17World'
